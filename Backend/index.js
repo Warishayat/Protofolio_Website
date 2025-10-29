f16b2a062aa1e2be4b70cea757393e53971bcb05
waris hayat <warishayat666@gmail.com>
2025-10-29 16:38:49 +0500

    final touches

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const ConnectDB = require("./Config/db");
 const Admin_router = require('./Router/AdminAuth');
 const Project_router = require('./Router/ProjectRoute');
 const Message_router = require("./Router/MessageRoute");
 const CV_router = require("./Router/CvRoute");
-const dotenv = require('dotenv').config()
-
 const app = express()
 
+const dotenv = require('dotenv').config()
+app.use(cors({
+  origin: '*',        
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json())
 const PORT = process.env.PORT || 8000;
+
 ConnectDB();
 
 
