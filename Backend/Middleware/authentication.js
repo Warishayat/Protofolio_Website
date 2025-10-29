9d88f8c80eed99f29c2f9511ae88623cfc6da590
waris hayat <warishayat666@gmail.com>
2025-10-29 17:54:33 +0500

    done

--- Backend/Middleware/authentication.js
+++ Backend/Middleware/authentication.js
@@ -2,23 +2,17 @@ const jwt = require("jsonwebtoken");
 
 const authentication = (req, res, next) => {
   const authHeader = req.headers.authorization || req.headers.Authorization;
-
-  console.log("Authorization Header:", authHeader);
-
   if (!authHeader?.startsWith("Bearer ")) {
     return res.status(401).json({ success: false, message: "No token provided" });
   }
 
   const token = authHeader.split(" ")[1];
-  console.log("Token received:", token); 
-
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     console.log("Decoded token:", decoded);
     req.user = decoded;
     next();
   } catch (error) {
-    console.error("Token verification failed:", error.message);
     return res.status(403).json({ success: false, message: "Invalid or expired token." });
   }
 };
